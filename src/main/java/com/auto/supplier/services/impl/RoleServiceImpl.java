911a5d5801efc5f1959d273c82afe8c300f9a7d0
shams.nalband.thabrez@sap.com <shams.nalband.thabrez@sap.com>
2019-10-08 20:05:19 +0530

    Corrected comment

--- src/main/java/com/auto/supplier/services/impl/RoleServiceImpl.java
+++ src/main/java/com/auto/supplier/services/impl/RoleServiceImpl.java
@@ -22,7 +22,7 @@ public class RoleServiceImpl implements RoleService {
   }
 
   @Override
-  // Should not be secured as this is pre-login to create user/org
+  // Should not be secured as this is pre-login to create user
   public Optional<RoleEntity> findByUniqueName(String uniqueName) {
     return roleRepository.findByUniqueName(uniqueName);
   }
