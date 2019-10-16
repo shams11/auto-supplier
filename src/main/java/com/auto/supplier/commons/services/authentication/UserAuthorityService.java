de5f0ec5dfd6b5f20874171c37bc26c8506f2aaa
shams.nalband.thabrez@sap.com <shams.nalband.thabrez@sap.com>
2019-10-16 15:39:03 +0530

    Removed redundant classes UserAuthorityService.java and UserEntityAuthorityService.java

--- src/main/java/com/auto/supplier/commons/services/authentication/UserAuthorityService.java
+++
@@ -1,8 +0,0 @@
-package com.auto.supplier.commons.services.authentication;
-
-import org.springframework.security.core.GrantedAuthority;
-import java.util.List;
-
-public interface UserAuthorityService {
-  List<GrantedAuthority> getAuthorities(Object var1);
-}
